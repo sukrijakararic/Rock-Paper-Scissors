91d8fc4300449b3cc7ee5d499d5da0ea7e10ba87
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-02-02 20:16:35 +0100

    added more styling and optimized js file

--- index.js
+++ index.js
@@ -6,41 +6,34 @@ const letsPlay = (handSign) => {
   let user = document.getElementById("user");
   let comp = document.getElementById("comp");
 
+  user.innerText = handSign;
+  comp.innerText = computerRandom;
+
   if (handSign == "✊" && computerRandom == "✌️") {
-    user.innerText = handSign;
-    comp.innerText = computerRandom;
     winner.innerText = "Good job, you win!";
   } else if (computerRandom == "✋" && handSign == "✊") {
-    user.innerText = handSign;
-    comp.innerText = computerRandom;
-    winner.innerText = "You lost scrub get gud";
+    winner.innerText = "You lost!";
   } else if (handSign == "✊" && computerRandom == "✊") {
-    user.innerText = handSign;
-    comp.innerText = computerRandom;
     winner.innerText = "It's a tie!";
   } else if (handSign == "✋" && computerRandom == "✊") {
-    user.innerText = handSign;
-    comp.innerText = computerRandom;
     winner.innerText = "Good job, you win!";
-  } else if (computerRandom == "✌️" && handSign == "✋") { 
-    user.innerText = handSign;
-    comp.innerText = computerRandom;
-    winner.innerText = "You lost scrub get gud";
+  } else if (computerRandom == "✌️" && handSign == "✋") {
+    winner.innerText = "You lost!";
   } else if (handSign == "✋" && computerRandom == "✋") {
-    user.innerText = handSign;
-    comp.innerText = computerRandom;
     winner.innerText = "It's a tie!";
   } else if (handSign == "✌️" && computerRandom == "✋") {
-    user.innerText = handSign;
-    comp.innerText = computerRandom;
     winner.innerText = "Good job, you win!";
   } else if (computerRandom == "✊" && handSign == "✌️") {
-    user.innerText = handSign;
-    comp.innerText = computerRandom;
-    winner.innerText = "You lost scrub get gud";
+    winner.innerText = "You lost!";
   } else if (handSign == "✌️" && computerRandom == "✌️") {
-    user.innerText = handSign;
-    comp.innerText = computerRandom;
     winner.innerText = "It's a tie!";
+  }
+
+  if (winner.innerText == "Good job, you win!") {
+    winner.style.color = "green";
+  } else if (winner.innerText == "You lost!") {
+    winner.style.color = "red";
+  } else if (winner.innerText == "It's a tie!") {
+    winner.style.color = "white";
   }
 };
